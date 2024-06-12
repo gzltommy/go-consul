2f2b10a4f9a9ca527161eb5a0d9636defbd82946
gouzuoluo <1046292959@qq.com>
2024-06-12 22:19:10 +0800

    1

--- 1.register/main.go
+++ 1.register/main.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"fmt"
-	consulapi "github.com/hashicorp/consul/api"
+	api "github.com/hashicorp/consul/api"
 	"net/http"
 )
 
 const (
-	consulAddress = "192.168.24.147:8500"
-	localIp       = "192.168.24.117"
-	localPort     = 81
+	consulAgentAddress = "127.0.0.1:8500"
+	localIp            = "192.168.3.9"
+	localPort          = 81
 )
 
 func consulRegister() {
 	// 获得一个 consul 客户端
-	client, err := consulapi.NewClient(&consulapi.Config{Address: consulAddress})
+	client, err := api.NewClient(&api.Config{Address: consulAgentAddress})
 	if err != nil {
 		fmt.Println("consul client error : ", err)
 		return
 	}
 
 	// 创建注册到 consul 的服务
-	service := &consulapi.AgentServiceRegistration{
+	service := &api.AgentServiceRegistration{
 		ID:      "337",
 		Name:    "service337",
 		Tags:    []string{"testService"},
 		Port:    localPort,
 		Address: localIp,
-		Check: &consulapi.AgentServiceCheck{
-			Interval:                       "5s",
-			Timeout:                        "5s",
-			HTTP:                           fmt.Sprintf("http://%s:%d/", localIp, localPort),
-			DeregisterCriticalServiceAfter: "30s", // 故障检查失败 30s 后 consul自动将注册服务删除
-		},
+		//Check: &api.AgentServiceCheck{
+		//	Interval:                       "5s",
+		//	Timeout:                        "5s",
+		//	HTTP:                           fmt.Sprintf("http://%s:%d/", localIp, localPort),
+		//	DeregisterCriticalServiceAfter: "30s", // 故障检查失败 30s 后 consul自动将注册服务删除
+		//},
 	}
 
 	// 注册服务到 consul
--- 2.find/main.go
+++ 2.find/main.go
@@ -2,39 +2,44 @@ package main
 
 import (
 	"fmt"
-	consulapi "github.com/hashicorp/consul/api"
+	api "github.com/hashicorp/consul/api"
+	"time"
 )
 
 const (
-	consulAgentAddress = "192.168.24.147:8500"
+	consulAgentAddress = "127.0.0.1:8500"
 )
 
-// 从 consul 中发现服务
+// ConsulFindServer 从 consul 中发现服务
 func ConsulFindServer() {
 	// 创建连接 consul 服务配置
-	config := consulapi.DefaultConfig()
-	config.Address = consulAgentAddress
-	client, err := consulapi.NewClient(config)
+	client, err := api.NewClient(&api.Config{Address: consulAgentAddress})
 	if err != nil {
 		fmt.Println("consul client error : ", err)
+		return
 	}
 
 	// 获取指定 service
 	service, _, err := client.Agent().Service("337", nil)
-	if err == nil {
-		fmt.Println(service.Address)
-		fmt.Println(service.Port)
+	if err != nil {
+		fmt.Println("Agent().Service fail", err)
+		return
 	}
 
-	//只获取健康的 service
-	//serviceHealthy, _, err := client.Health().Service("service337", "", true, nil)
-	//if err == nil{
-	//	fmt.Println(serviceHealthy[0].Service.Address)
-	//}
+	fmt.Println(service.Address)
+	fmt.Println(service.Port)
 
+	//只获取健康的 service
+	serviceHealthy, _, err := client.Health().Service("service337", "", true, nil)
+	if err != nil {
+		fmt.Println("Health().Service fail", err)
+		return
+	}
+	fmt.Println(serviceHealthy[0].Service.Address)
+	fmt.Println(serviceHealthy[0].Service.Port)
 }
 
 func main() {
 	ConsulFindServer()
-	select {}
+	time.Sleep(time.Hour)
 }
