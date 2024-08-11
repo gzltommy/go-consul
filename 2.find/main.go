ca61500236ebf139bb193716c61543bbd83174f1
gouzuoluo <1046292959@qq.com>
2024-08-11 18:05:20 +0800

    服务发现

--- 2.find/main.go
+++ 2.find/main.go
@@ -19,16 +19,14 @@ func ConsulFindServer() {
 	}
 
 	// 获取指定 ID service
-	//{
-	//	service, _, err := client.Agent().Service("337", nil)
-	//	if err != nil {
-	//		fmt.Println("Agent().Service fail", err)
-	//		return
-	//	}
-	//
-	//	fmt.Println(service.Address)
-	//	fmt.Println(service.Port)
-	//}
+	{
+		service, _, err := client.Agent().Service("c9dc568361a04350a69f864597e1e9cd", nil)
+		if err != nil {
+			fmt.Println("Agent().Service fail", err)
+			return
+		}
+		fmt.Printf("server:%s:%d\n", service.Address, service.Port)
+	}
 
 	// 获取指定服务的所有健康的节点 server
 	{
@@ -63,6 +61,17 @@ func ConsulFindServer() {
 		}
 		for _, v := range allServer {
 			fmt.Printf("%s: %s:%d\n", v.Service, v.Address, v.Port)
+		}
+	}
+	// 获取 consul 上的所有 server name
+	{
+		allServer, _, err := client.Catalog().Services(nil)
+		if err != nil {
+			fmt.Println("Agent().Services fail", err)
+			return
+		}
+		for name, tags := range allServer {
+			fmt.Printf("%s:%v\n", name, tags)
 		}
 	}
 
